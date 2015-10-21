00297b35c0c433e7caf3502c5402343e2812482b
Braxton Schafer <braxton.schafer@gmail.com>
2015-10-20 19:44:36 -0500

    Support specifying port as command-line flag on startup

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":80", router))
+	port := flag.Int("port", 8080, "Port to run on")
+	flag.Parse()
+
+	if *port < 1 || *port > 65536 {
+		log.Fatal(string(*port) + " is not a valid port number. Exiting.")
+	}
+
+	portString := ":" + strconv.Itoa(*port)
+
+	fmt.Printf("Starting server on port %s", portString)
+
+	log.Fatal(http.ListenAndServe(portString, router))
 }
